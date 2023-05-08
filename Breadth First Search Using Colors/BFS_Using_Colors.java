f798e8ac4afacae1b70b51181814d03b612a824d
Tanvir Ahmed <132837418+tanvirahmed1732@users.noreply.github.com>
2023-05-09 01:02:08 +0600

    Breadth First Search Using Colors
    In this code, we first define a 2D array adjMatrix to represent the adjacency matrix of the graph. We then call the bfs method with the matrix and the source vertex as input.
    
    Inside the bfs method, we first initialize the arrays color, prev, and d with default values, and create an empty queue q.
    
    We then mark the source vertex as GREY, set its distance to 0, and add it to the queue. We then enter a loop that continues until the queue is empty. In each iteration of the loop, we dequeue a vertex u from the queue, and iterate over its neighbors v. If a neighbor v is white (unvisited), we mark it as GREY, set its distance and parent, and add it to the queue. After all the neighbors of u have been processed, we mark u as BLACK.
    
    Finally, we output the values of color, prev, and d.
    
    Note that in this code, we are using the string "WHITE", "GREY", and "BLACK" to represent the colors of vertices. We are also using the value Integer.MAX_VALUE to represent an infinite distance.

---
+++ Breadth First Search Using Colors/BFS_Using_Colors.java
@@ -0,0 +1,50 @@
+package com.mycompany.mavenproject2;
+
+import java.util.*;
+
+public class BFS_Using_Colors {
+
+    public static void main(String[] args) {
+        int[][] adjMatrix = {{0, 1, 1, 0, 1},
+                            {1, 0, 1, 0, 1},
+                            {1, 1, 0, 1, 0},
+                            {0, 0, 1, 0, 1},
+                            {1, 1, 0, 1, 0}};
+        bfs(adjMatrix, 0);
+    }
+
+    public static void bfs(int[][] adjMatrix, int source) {
+        int n = adjMatrix.length;
+        String[] color = new String[n];
+        int[] prev = new int[n];
+        int[] d = new int[n];
+
+        // initialize
+        for (int i = 0; i < n; i++) {
+            color[i] = "WHITE";
+            prev[i] = -1;
+            d[i] = Integer.MAX_VALUE;
+        }
+
+        Queue<Integer> q = new LinkedList<>();
+        color[source] = "GREY";
+        d[source] = 0;
+        prev[source] = -1;
+        q.add(source);
+
+        while (!q.isEmpty()) {
+            int u = q.poll();
+            System.out.print(u + " ");
+            for (int v = 0; v < n; v++) {
+                if (adjMatrix[u][v] == 1 && color[v] == "WHITE") {
+                    color[v] = "GREY";
+                    d[v] = d[u] + 1;
+                    prev[v] = u;
+                    q.add(v);
+                }
+            }
+            color[u] = "BLACK";
+        }
+    }
+}
+
