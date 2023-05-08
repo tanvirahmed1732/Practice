22e8cde3741bd14b3ec5f5a4228409a90aabefa6
Tanvir Ahmed <132837418+tanvirahmed1732@users.noreply.github.com>
2023-05-09 00:44:25 +0600

    Detect the cycle in a graph using BFS
    This program takes an adjacency matrix representation of a graph as input and uses BFS to detect if a cycle exists starting from a specified start node. It returns true if a cycle is detected and false otherwise. The algorithm works by using a visited array to keep track of nodes that have been visited during BFS. If a node is already visited and it is not the parent of the current node being visited, then a cycle is detected. The program uses a queue to perform BFS traversal and returns the result as a boolean value.
    
    Overall, this program and algorithm are a useful way to detect cycles in a graph using BFS. By keeping track of visited nodes and checking for cycles during traversal, the program is able to detect cycles in a variety of graphs.

---
+++ Detect the cycle in a graph using BFS/CycleDetection.java
@@ -0,0 +1,43 @@
+
+
+package com.mycompany.mavenproject2;
+
+import java.util.*;
+
+public class CycleDetection{
+    
+    public boolean detectCycle(int start, int[][] adjMat) {
+        Queue<Integer> q = new LinkedList<>();
+        boolean[] v = new boolean[adjMat.length];
+        q.add(start);
+        v[start] = true;
+        while(!q.isEmpty()) {
+            int CN = q.poll();
+            for(int n = 0; n < adjMat.length; n++) {
+                if(adjMat[CN][n] == 1) {
+                    if(!v[n]) {
+                        v[n] = true;
+                        q.add(n);
+                    } else if(n != 0) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+    
+    public static void main(String[] args) {
+        int[][] adjMat = {   {0, 1, 0, 0},
+                             {0, 0, 1, 0},
+                             {0, 0, 0, 1},
+                             {1, 0, 0, 0}};
+        CycleDetection CD = new CycleDetection();
+        boolean cycleExists = CD.detectCycle(0, adjMat);
+        if(cycleExists) {
+            System.out.println("Cycle detected!");
+        } else {
+            System.out.println("No cycle detected.");
+        }
+    }
+}
