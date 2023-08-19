fa0a3b5e7f84f896403d905ad79f74c50523b892
Tanvir Ahmed <132837418+tanvirahmed1732@users.noreply.github.com>
2023-08-19 21:37:01 +0600

    basic_calculator.py
    This is the calculator that I program the first time using Python language.

---
+++ Python Practice/basic_calculator.py
@@ -0,0 +1,16 @@
+a = 4
+b = 2
+add = a + b
+print("Addition of", a, "and", b, "is", add)
+mul = a * b
+print("Multiplication of", a, "and", b, "is", mul)
+sub = a - b
+print("Substraction of", a, "and", b, "is", sub)
+div = a / b
+print("Division of", a, "and", b, "is", div)
+exp = a ** b
+print("Exponential of", a, "and", b, "is", exp)
+mod = a % b
+print("Modulus of", a, "and", b, "is", mod)
+floordiv = a // b
+print("Flood Division of", a, "and", b, "is", floordiv)
