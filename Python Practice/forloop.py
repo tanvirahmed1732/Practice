91face86d9992e29d9e981f62ddd742150002b86
Tanvir Ahmed <132837418+tanvirahmed1732@users.noreply.github.com>
2023-08-20 22:28:30 +0600

    Add files via upload

---
+++ Python Practice/forloop.py
@@ -0,0 +1,11 @@
+name = 'Tanvir'
+for i in name:
+  print(i, end="")
+print()
+arr = [0,1,2,3,4,5,6,7,8,9]
+print(len(arr))
+print(arr[5])
+# for i in arr:
+#   print(i, end=' ')
+for i in range(0,10):
+  print(arr[i], end=' ')
