618350620be7c32650f76c7942cb2958bf815aae
Tanvir Ahmed <132837418+tanvirahmed1732@users.noreply.github.com>
2023-08-20 22:50:20 +0600

    Add files via upload

---
+++ Python Practice/whileloop.py
@@ -0,0 +1,21 @@
+# i = int(input("Enter the number: "))
+# while(i<=38):
+#   i = int(input("Enter the number: "))
+#   print(i)
+
+# print("Done with the loop")
+
+count = 5;
+while (count > 0):
+  print(count);
+  count = count - 1;
+else:
+  print("The loop is over.");
+
+# Do while lool
+n = int(input())
+while (True):
+  print(n);
+  n = n - 1;
+  if(n<=0):
+    break
