0e88c3eac6083113488c1763133a02431aed4152
Tanvir Ahmed <132837418+tanvirahmed1732@users.noreply.github.com>
2024-03-13 22:16:58 +0600

    Pattern2
    *****
    ****
    ***
    **
    *
    **
    ***
    ****
    *****

---
+++ Python Practice/pattern2.py
@@ -0,0 +1,12 @@
+for i in range(5, 0, -1):
+    for j in range(1, i + 1):
+        print("*", end="")
+    print()
+
+
+for i in range(2, 6):
+    for j in range(1, i + 1):
+        print("*", end="")
+    print()
+
+
