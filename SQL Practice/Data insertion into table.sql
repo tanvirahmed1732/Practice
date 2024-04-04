f83206b7ce785502b95d863c71bba039735eee16
Tanvir Ahmed <132837418+tanvirahmed1732@users.noreply.github.com>
2024-04-04 22:51:22 +0600

    Data insertion into table

---
+++ SQL Practice/Data insertion into table.sql
@@ -0,0 +1,2 @@
+/* Write a query to insert the below mentioned employee details to the table 'employee' */
+insert into employee(Employee_id,Employee_name,Department) values(4,'Marcus Garcia','Product'),(5,'Samantha Park','Hr');
