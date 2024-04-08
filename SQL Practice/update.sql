18d1e74e62e060181a11fcfad7c5a9717fb8081f
Tanvir Ahmed <132837418+tanvirahmed1732@users.noreply.github.com>
2024-04-08 22:13:28 +0600

    learning update keyword

---
+++ SQL Practice/update.sql
@@ -0,0 +1,3 @@
+/*Write a query to set the Department as 'HR', for the employee with employee_id 2 to the existing table employee. */
+update employee set Department = 'HR' where Employee_id = 2;
+select * from employee;
