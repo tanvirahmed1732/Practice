11806093f06f941985097dd1c39655c6cd084618
Tanvir Ahmed <132837418+tanvirahmed1732@users.noreply.github.com>
2024-04-11 10:54:08 +0600

    Delete from

---
+++ SQL Practice/Delete from.sql
@@ -0,0 +1,5 @@
+/* Write a query which does the following
+- Delete all rows in the employee table whose Department is 'Hr'.
+- Output all the entires of the table. */
+delete from employee where Department = 'Hr';
+select * from employee;
