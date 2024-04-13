5e09e1fa1f73ff6123b31b017f41055e75a8a1cd
Tanvir Ahmed <132837418+tanvirahmed1732@users.noreply.github.com>
2024-04-13 20:23:32 +0600

    Delete from table

---
+++ SQL Practice/Delete from table.sql
@@ -0,0 +1,6 @@
+/* Write a query which does the following
+- Delete the row where the department is Client.
+- Output the entire table.
+*/
+delete from employee where Department = 'Client';
+select * from employee;
