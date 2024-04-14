3fda3ce8462627b0a0cf2ab33baaa54331a3042e
Tanvir Ahmed <132837418+tanvirahmed1732@users.noreply.github.com>
2024-04-14 22:21:41 +0600

    Debug

---
+++ SQL Practice/Debug.sql
@@ -0,0 +1,8 @@
+/* Debug this query to run the problem successfully. 
+- Don’t worry about the actual values as long as you get the query to run. */
+
+INSERT INTO employee (Id,Name,Age,Address)
+VALUES  (1, 'John Smith', 25,  '123 Main St'),
+        (2, 'Sarah Johnson', 30,'456 Broadway'),
+        (3, 'Michael Brown', 45,  '124 Main St'),
+        (4, 'Jessica Davis', 28,  '321 Elm St');
