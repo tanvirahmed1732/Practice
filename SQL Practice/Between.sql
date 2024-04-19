bb6b95f3dfb09650420ab739e9f7bf01a0677092
Tanvir Ahmed <132837418+tanvirahmed1732@users.noreply.github.com>
2024-04-19 17:03:12 +0600

    Between

---
+++ SQL Practice/Between.sql
@@ -0,0 +1,5 @@
+/* Write a query to do the following
+- Output all entries from the table
+- Where passenger_name between 'A' and 'D' */
+
+select * from Flights where passenger_name BETWEEN 'A' AND 'D';
